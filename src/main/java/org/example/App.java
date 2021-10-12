2b369cfb73ba935698dbc1cbb324444d6887fd3c
fjux <erikalfredsson1@gmail.com>
2021-10-12 16:45:20 +0200

    first exercise

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,16 @@
 package org.example;
 
 
-public class App 
+import java.util.Arrays;
+
+public class App
 {
     public static void main( String[] args ) {
-
+        printArray();
     }
+    public static void printArray(){
+        int[] numbers = {1, 2, 3, 4, 5, 66, 7, 99, 8, 10};
+        System.out.println(Arrays.toString(numbers));
+    }
+
 }
